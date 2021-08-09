dd723cc9618f94be44782228ab486ac6ae58f063
igoreshirokov <igoreshirokov@yandex.ru>
2021-08-09 20:27:43 +0300

    e

--- contstants.js
+++ contstants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:3000/';
+// export const BASE_URL = 'http://localhost:3000/';
+export const BASE_URL = 'http://81.177.48.218:3000/';
 export const LINKS = {
     vk: '#',
     instagram: '#',
